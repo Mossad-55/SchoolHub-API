543fd1c018e131ffb0fc4150eb73ea332380652f
Mossad <mosad55522@gmail.com>
2025-11-15 17:15:58 +0200

    Added RepositoryConfiguration with User Tb

---
+++ SchoolHubAPI.Entities/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace SchoolHubAPI.Entities.Entities;
+
+public class User : IdentityUser<Guid>  
+{
+    public DateTime? CreatedDate { get; set; }
+    public DateTime? UpdatedDate { get; set; }
+
+    // Roles Profiles
+
+}
---
+++ SchoolHubAPI.Repository/RepositoryContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SchoolHubAPI.Entities.Entities;
+
+namespace SchoolHubAPI.Repository;
+
+public class RepositoryContext : IdentityDbContext<User, IdentityRole<Guid>, Guid>
+{
+    public RepositoryContext(DbContextOptions options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Add Configuration Data and Roles.
+
+    }
+
+    // DbSets
+
+}
