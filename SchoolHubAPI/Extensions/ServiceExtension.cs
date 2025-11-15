3881ba686c55756f26afad2ccff4e7b109a143a1
Mossad <mosad55522@gmail.com>
2025-11-15 17:19:42 +0200

    Added Extension Method for Configuring Services

---
+++ SchoolHubAPI/Extensions/ServiceExtension.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SchoolHubAPI.Repository;
+
+namespace SchoolHubAPI.Extensions;
+
+public static class ServiceExtension
+{
+    // Add services to the container.
+    
+    // CORS Configuration
+    public static void ConfigureCors(this IServiceCollection services) =>
+        services.AddCors(options =>
+        {
+            options.AddPolicy("CorsPolicy", builder =>
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader()
+                       .WithExposedHeaders("X-Pagination"));
+        });
+
+    // Sql Connection Configuration
+    public static void ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration) =>
+        services.AddDbContext<RepositoryContext>(opts =>
+            opts.UseSqlServer(configuration.GetConnectionString("sqlConnection")));
+
+
+}
