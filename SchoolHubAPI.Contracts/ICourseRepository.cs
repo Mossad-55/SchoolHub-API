bd825d448577e365ed88f92158a3159856dea32a
Mossad <mosad55522@gmail.com>
2025-11-20 02:49:57 +0200

    Added ICourseRepository.cs interface

---
+++ SchoolHubAPI.Contracts/ICourseRepository.cs
@@ -0,0 +1,14 @@
+﻿using SchoolHubAPI.Entities.Entities;
+using SchoolHubAPI.Shared.RequestFeatures;
+
+namespace SchoolHubAPI.Contracts;
+
+public interface ICourseRepository
+{
+    Task<PagedList<Course>>? GetAllCoursesAsync(RequestParameters requestParameters, bool trackChanges);
+    Task<Course?> GetCourseAsync(Guid id, bool trackChanges);
+    Task<bool> CheckIfCourseExists(Guid departmentId, string code, bool trackChanges);
+    void DeleteCourse(Course course);
+    void CreateCourse(Course course);
+    void UpdateCourse(Course course);
+}
